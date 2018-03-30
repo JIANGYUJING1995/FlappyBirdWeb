23f3ce581efc6fd57a76f7d5dbf9be8ac1962c99
jiangyj <a10885744@163.com>
2018-03-31 05:00:14 +0800

    创建背景图片前

--- Director.js
+++ Director.js
@@ -1,3 +1,12 @@
 export class Director {
-    
+    constructor(){
+      console.log("构造器初始化")
+    }
+
+    static getInstance(){
+        if(!Director.instance){
+          Director.instance = new Director()
+        }
+        return Director.instance;
+    }
 }
--- Main.js
+++ Main.js
@@ -1,4 +1,16 @@
 import { ResourceLoader } from "./js/base/ResourceLoader";
+import { Director } from "./Director.js";
+const RES_PATH = '../res/';
+const RES_EXTE = '.png'
+const RES_IMGS =  {
+    'background.png': 'background.png',
+    'birds.png': 'birds.png',
+    'land.png': 'land.png',
+    'pie_down.png': 'pie_down.png',
+    'pie_up.png': 'pie_up.png',
+    'start_button.png': 'start_button.png',
+}
+
 
 //初始化整个游戏的精灵，作为游戏开始的入口
 export class Main {
@@ -7,9 +19,24 @@ export class Main {
         new ResourceLoader();
         this.canvas =  wx.createCanvas();
         this.ctx = this.canvas.getContext("2d");
+        console.log(this.ctx)
         const loader = ResourceLoader.create();
         // 让资源加载完才进行渲染
         loader.onLoaded(map => this.onResourceFirstLoaded(map))
+        // Director.getInstance();
+
+        let image = wx.createImage;
+        image.src = `${RES_PATH}${RES_IMGS["background.png"]}`
+        console.log(image.src)
+        this.ctx.drawImage(
+          image,
+          0,
+          0,
+          image.width,
+          image.height,
+          0,
+          0
+        )
     }
 
     onResourceFirstLoaded(map){
