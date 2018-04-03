79f95cc554d235823110e350fc4c3aad701cc690
jiangyj <a10885744@163.com>
2018-04-04 01:11:36 +0800

    封装了Sprite.js

--- Main.js
+++ Main.js
@@ -1,20 +1,8 @@
 import { ResourceLoader } from "./js/base/ResourceLoader.js";
 import { Director } from "./Director.js";
-const RES_PATH = '../res/';
-const RES_EXTE = '.png'
-const RES_IMGS =  {
-    'background.png': 'background.png',
-    'birds.png': 'birds.png',
-    'land.png': 'land.png',
-    'pie_down.png': 'pie_down.png',
-    'pie_up.png': 'pie_up.png',
-    'start_button.png': 'start_button.png',
-}
-
+import { BackGround } from "./js/runtime/BackGround.js";
 //初始化整个游戏的精灵，作为游戏开始的入口
 export class Main {
-
-
     constructor(){
         console.log("执行man")
         // 创建一个加载器
@@ -28,23 +16,13 @@ export class Main {
         // 让资源加载完才进行渲染-通过把回调函数传入onLoaded->再由onLoaded进行调用
         loader.onLoaded(map => this.onResourceFirstLoaded(map))
         // Director.getInstance();
-
-        let image = new Image();
-        // image.src = `${RES_PATH}${RES_IMGS["background.png"]}`;
-        image.src = '../res/background.png';
-        image.onload = () =>{
-            this.ctx.drawImage(
-                image,
-                0, 0, 
-                image.width,image.height,
-                0,0,
-                image.width,image.height
-            );
-        };
         
     }
 
     onResourceFirstLoaded(map){
-        console.log(map)
+        // console.log(map)
+        let image = new Image();
+        let background = new BackGround(this.ctx,map.get('background'));
+        background.draw();
     }
 }
--- js/base/Sprite.js
+++ js/base/Sprite.js
@@ -1,4 +1,57 @@
 //精灵的基类，负责初始化精灵加载的资源和大小以及位置
 export class Sprite {
-    
+    constructor(
+        ctx = null,
+        img = null, //绘制的对象
+        srcX = 0,   //剪裁的x,y
+        srcY = 0,
+        srcW = 0,   //剪裁的宽高
+        srcH = 0,
+        x = 0, y = 0,  // 布局到canvas的坐标
+        width = 0, height = 0 // 布局到canvas的宽度高度
+    ){
+        this.ctx = ctx;
+        this.img = img;
+        this.srcX = srcX;
+        this.srcY = srcY;
+        this.srcW = srcW;
+        this.srcH = srcH;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * img 传入Image对象
+     * srcX 要剪裁的起始X坐标
+     * srcY 要剪裁的起始Y坐标
+     * srcW 剪裁的宽度
+     * srcH 剪裁的高度
+     * x 放置的x坐标
+     * y 放置的y坐标
+     * width 要使用的宽度
+     * height 要使用的高度
+     */
+    draw(img = this.img,
+        srcX = this.srcX,
+        srcY = this.srcY,
+        srcW = this.srcW,
+        srcH = this.srcH,
+        x = this.x,
+        y = this.y,
+        width = this.width,
+        height = this.height) {
+       this.ctx.drawImage(
+           img,
+           srcX,
+           srcY,
+           srcW,
+           srcH,
+           x,
+           y,
+           width,
+           height
+       );
+   }
 }
